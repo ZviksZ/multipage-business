cd10598ebe06a28b8f8afd1f10bb254b49e7cba2
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-08-03 17:50:18 +0300

    Страница о компании - фдаптив 30%

--- src/js/components/fullscreen-scroll/index.js
+++ src/js/components/fullscreen-scroll/index.js
@@ -44,8 +44,6 @@ export class FullscreenScroll {
 
       window.addEventListener('resize', this.resize)
 
-      this.downBtn.addEventListener('click', this.initFirstSectionDownBtn)
-
 
    }
 
@@ -190,44 +188,48 @@ export class FullscreenScroll {
    animateBlockOnScroll() {
       const $block = document.getElementById('fullscreen-section_' + this.currentSection);
       const $blockId = '#fullscreen-section_' + this.currentSection;
-      const animate = $block.dataset.animate;
-      if (animate) {
-         $block.querySelectorAll('.circle-item').forEach(function (item) {
-            let bar = new ProgressBar.Circle(item, {
-               strokeWidth: 2,
-               easing: 'easeInOut',
-               duration: 1400,
-               color: '#ba1946',
-               trailColor: 'rgba(255,255,255,0.3)',
-               trailWidth: 2,
-               svgStyle: null
-            });
-
-            bar.animate(1.0);
-
-            item.querySelectorAll('.counter').forEach(function (counter) {
-               counter.classList.add('circle-active')
-            })
-         })
-
-         if ($block.querySelectorAll('.counter').length) {
-
-            $($blockId).find('.circle-active').each(function () {
-               $(this).prop('Counter',0).animate({
-                  Counter: $(this).text()
-               }, {
-                  duration: 1500,
-                  easing: 'swing',
-                  step: function (now) {
-                     $(this).text(Math.ceil(now));
-                  }
+      if ($block) {
+         const animate = $block.dataset.animate;
+         if (animate) {
+            $block.querySelectorAll('.circle-item').forEach(function (item) {
+               let bar = new ProgressBar.Circle(item, {
+                  strokeWidth: 2,
+                  easing: 'easeInOut',
+                  duration: 1400,
+                  color: '#ba1946',
+                  trailColor: 'rgba(255,255,255,0.3)',
+                  trailWidth: 2,
+                  svgStyle: null
                });
-            });
+
+               bar.animate(1.0);
+
+               item.querySelectorAll('.counter').forEach(function (counter) {
+                  counter.classList.add('circle-active')
+               })
+            })
+
+            if ($block.querySelectorAll('.counter').length) {
+
+               $($blockId).find('.circle-active').each(function () {
+                  $(this).prop('Counter',0).animate({
+                     Counter: $(this).text()
+                  }, {
+                     duration: 1500,
+                     easing: 'swing',
+                     step: function (now) {
+                        $(this).text(Math.ceil(now));
+                     }
+                  });
+               });
+            }
+
+
+            $block.removeAttribute('data-animate');
          }
-
-
-         $block.removeAttribute('data-animate');
       }
+
+
    }
 
    animateBlockMobile() {
@@ -265,150 +267,5 @@ export class FullscreenScroll {
       }
    }
 
-
-   /*scrollTo(pageIndex, isDown) {
-      if (this.index === pageIndex || pageIndex === this.pages.length) {
-         return false
-      }
-      this.index = pageIndex;
-
-      let containerTranslate = this.getContainerTranslateY();
-      let target;
-
-      if (containerTranslate === 0 && !isDown) {
-         return false
-      }
-
-      if (this.pages[pageIndex].offsetHeight <= window.innerHeight && isDown) {
-         target = - this.pages[pageIndex].offsetHeight + containerTranslate;
-      } else if (this.pages[pageIndex].offsetHeight <= window.innerHeight && !isDown) {
-         target = this.pages[pageIndex + 1].offsetHeight + containerTranslate;
-      }
-
-      const _ticker = this.ticker = Date.now();
-      this.scrolling = true;
-
-      const timer = setInterval(() => {
-         if (this.ticker !== _ticker) {
-            clearInterval(timer);
-            return;
-         }
-
-         const source = this.getContainerTranslateY();
-         const nextStep = source + (target > source ? Math.ceil : Math.floor)((target - source) * 0.05);
-
-         this.container.style.transform = 'translateY(' + nextStep +'px)';
-
-         if (nextStep === target) {
-            clearInterval(timer);
-            this.scrolling = false;
-         }
-      }, 6);
-   }
-
-   changeHeaderOnScroll() {
-      if (this.curr !== 0) {
-         this.$header.classList.add(this.headerClassName);
-      } else {
-         this.$header.classList.remove(this.headerClassName);
-      }
-   }
-
-   getContainerTranslateY() {
-      return Number.parseInt(window.getComputedStyle(this.container).transform.split(', ')[5]) || 0
-   }
-
-   startAnimateBlock(pageIndex) {
-      if (this.pages[this.curr].classList.contains('animate-section') && !this.$animatedBLocks.includes(pageIndex)) {
-         this.pages[this.curr].querySelectorAll('.circle-item').forEach(function (item) {
-            let bar = new ProgressBar.Circle(item, {
-               strokeWidth: 2,
-               easing: 'easeInOut',
-               duration: 1400,
-               color: '#ba1946',
-               trailColor: 'rgba(255,255,255,0.3)',
-               trailWidth: 2,
-               svgStyle: null
-            });
-
-            bar.animate(1.0);
-
-            item.querySelectorAll('.counter').forEach(function (counter) {
-               counter.classList.add('circle-active')
-            })
-         })
-
-         if (this.pages[this.curr].querySelectorAll('.counter').length) {
-
-            $('.circle-active').each(function () {
-               $(this).prop('Counter',0).animate({
-                  Counter: $(this).text()
-               }, {
-                  duration: 1500,
-                  easing: 'swing',
-                  step: function (now) {
-                     $(this).text(Math.ceil(now));
-                  }
-               });
-            });
-         }
-
-         this.$animatedBLocks.push(pageIndex)
-      }
-   }
-
-   initAnimateBlockMobile() {
-      this.container.querySelectorAll('.circle-item').forEach(function (item) {
-         let bar = new ProgressBar.Circle(item, {
-            strokeWidth: 2,
-            easing: 'easeInOut',
-            duration: 1400,
-            color: '#ba1946',
-            trailColor: 'rgba(255,255,255,0.3)',
-            trailWidth: 2,
-            svgStyle: null
-         });
-
-         bar.animate(1.0);
-
-         item.querySelectorAll('.counter').forEach(function (counter) {
-            counter.classList.add('circle-active')
-         })
-      })
-
-      if (this.container.querySelectorAll('.counter').length) {
-
-         $('.circle-active').each(function () {
-            $(this).prop('Counter',0).animate({
-               Counter: $(this).text()
-            }, {
-               duration: 1500,
-               easing: 'swing',
-               step: function (now) {
-                  $(this).text(Math.ceil(now));
-               }
-            });
-         });
-      }
-   }
-
-   initFirstSectionDownBtn(e) {
-      e.preventDefault();
-
-      this.scrollTo(1, true)
-      this.curr = 1;
-
-      this.pages.forEach((page, index) => {
-         if (index === this.curr) {
-            page.classList.add('active');
-         } else {
-            page.classList.remove('active');
-         }
-      });
-
-      this.startAnimateBlock(1)
-
-      this.changeHeaderOnScroll()
-   }*/
 
 }
